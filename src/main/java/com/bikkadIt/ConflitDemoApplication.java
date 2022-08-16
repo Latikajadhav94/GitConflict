579d21ce449504ba89f40e9ac49424e27db0475d
LatikaJadhav94 <latikaj189@gmail.com>
2022-08-16 19:56:15 +0530

    Good morning

--- src/main/java/com/bikkadIt/ConflitDemoApplication.java
+++ src/main/java/com/bikkadIt/ConflitDemoApplication.java
@@ -8,6 +8,9 @@ public class ConflitDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConflitDemoApplication.class, args);
+		
+		
+		String Msg="good morning";
 	}
 
 }
